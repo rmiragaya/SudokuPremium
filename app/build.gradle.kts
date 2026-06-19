db83c0567dfc5ca493e4541bde017d9bdfff1d39
Rodrigo Miragaya <rodrigo.e.miragaya@gmail.com>
2026-06-19 11:11:36 -0300

    Bump application version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "ropa.miragaya.sudokumentor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
